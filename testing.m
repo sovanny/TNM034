38f547346685405793ea9ef37741a4052e77dc53
Axel Kollberg <axeko787@student.liu.se>
2018-11-29 12:19:03 +0100

    divide staffs into subimages

--- testing.m
+++ testing.m
@@ -3,8 +3,11 @@ images = dir('./Images_Training/*.jpg');
 %%
 result = [];
 for img = images'
-    
+    img
     %result = [result,tnm034(strcat(img.folder,'\',img.name))];
     result = [result,tnm034(strcat(img.folder,'/',img.name))];
     
 end
+
+%%
+tnm034('./Images_Training/Le_1_Example.jpg')
