dba73e9cb37c7e022399c7d2c29a7cb1a4c0bfdb
Sovanny <shuy@c-10-253-252-160.clients.eduroam.liu.se>
2018-11-29 09:00:44 +0100

    små ändringar

--- testing.m
+++ testing.m
@@ -4,6 +4,7 @@ images = dir('./Images_Training/*.jpg');
 result = [];
 for img = images'
     
-    result = [result,tnm034(strcat(img.folder,'\',img.name))];
+    %result = [result,tnm034(strcat(img.folder,'\',img.name))];
+    result = [result,tnm034(strcat(img.folder,'/',img.name))];
     
 end
